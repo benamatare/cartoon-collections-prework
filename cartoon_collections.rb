9fdaae0a6e99c44690aaf2f849bee6b8ea226b2c
Ben Matare <ben.a.matare@gmail.com>
2018-01-21 07:26:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,17 @@ def summon_captain_planet array# code an argument here
   array.map! {|value| value + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+
+  def long_planeteer_calls array
+    array.each do |value|
+      if value.length > 4
+      return true
+        elsif value.length < 4
+        return false
+        end
+    end
+  end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
